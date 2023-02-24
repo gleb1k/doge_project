cd666843f1a044bf49a5f86d8b93ed675bd06775
Gleb Gafeev <gleb.gafeev@mail.ru>
2023-02-25 01:02:07 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json',
+                'x-api-key': 'live_rOuqiEI0j2qAQUZMDnHDzegt5x6uP7PVA3EUdt8KY88WgGno5hxYumlDRGs2vusP'
+            },
+        };
+
+        fetch("https://api.thedogapi.com/v1/breeds?limit=10&page=0", requestOptions)
+        .then(response => response.json())
+        .then(data =>console.log(data));
+    }, [])
+
+
+    const [count, setCount] = useState(0)
+
+    function buttonClick() {
+        console.log("ТЫК")
+        setCount(count + 1)
+    }
+
+    return (
+        <div className="App">
+            <h1>App test fsedf {count}</h1>
+            <button onClick={buttonClick}>Буба</button>
+        </div>
+    );
+
 }
 
 export default App;
